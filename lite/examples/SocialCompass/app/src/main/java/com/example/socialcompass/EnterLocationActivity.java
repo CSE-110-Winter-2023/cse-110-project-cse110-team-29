318c8d0b3da729cb125a032f4bbbd91220ff9935
raymosun <sunraysfs@gmail.com>
2023-02-07 12:05:20 -0800

    Add basic activity for entering location

--- lite/examples/SocialCompass/app/src/main/java/com/example/socialcompass/EnterLocationActivity.java
+++ lite/examples/SocialCompass/app/src/main/java/com/example/socialcompass/EnterLocationActivity.java
@@ -2,17 +2,13 @@ package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EnterLocationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, EnterLocationActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_enter_location);
     }
 }
