dbe749723e2249fdf3065dc4953d1cfba456a770
raymosun <sunraysfs@gmail.com>
2023-02-07 12:41:54 -0800

    Add stubs for onClick methods

--- lite/examples/SocialCompass/app/src/main/java/com/example/socialcompass/EnterLocationActivity.java
+++ lite/examples/SocialCompass/app/src/main/java/com/example/socialcompass/EnterLocationActivity.java
@@ -3,6 +3,7 @@ package com.example.socialcompass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EnterLocationActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class EnterLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_location);
+    }
+
+    public void onSubmit(View view) {
+    }
+
+    public void onAddLocation(View view) {
     }
 }
