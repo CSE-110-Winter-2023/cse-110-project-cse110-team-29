baba6b4af206d821c61f51436a1305461637e9c4
Raymond Sun <30709485+raymosun@users.noreply.github.com>
2023-02-11 10:48:17 -0800

    Input interface (#27)
    Write initial input interface.
    
    It has an activity that takes label, latitude, and longitude and saves these for 1 location.
    
    * Initial Input interface
    
    * change name to label's name
    
    * Initial input backend progress
    
    * location service for basic data storage
    
    * Update so app takes 1 input and saves it
    
    ---------
    
    Co-authored-by: elllisonLi <yul054@ucsd.edu>
    Co-authored-by: dhylan01 <dhylan01@gmail.com>

---
+++ app/src/main/java/com/example/socialcompass/InputActivity.java
@@ -0,0 +1,65 @@
+package com.example.socialcompass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class InputActivity extends AppCompatActivity {
+    // private ArrayList<Location> locations;
+    private EditText labelInput;
+    private EditText latInput;
+    private EditText longInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        labelInput = findViewById(R.id.labelName);
+        latInput = findViewById(R.id.latitudeNum);
+        longInput = findViewById(R.id.longitudeNum);
+
+        // locations = new ArrayList<>();
+        // TODO: update locations with saved locations
+    }
+
+    public void saveLocation() {
+        // get a new Location from user input
+        // TODO: validate inputs
+        Location newLoc = new Location(
+                labelInput.getText().toString(),
+                Float.parseFloat(latInput.getText().toString()),
+                Float.parseFloat(longInput.getText().toString()));
+
+        // locations.add(newLoc);
+
+
+        // save locations
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("parentsLabel", newLoc.getLabel());
+        editor.putFloat("parentsLat", newLoc.getLatitude());
+        editor.putFloat("parentsLong", newLoc.getLongitude());
+        editor.apply();
+    }
+
+    public void onSaveAndContinue(View view) {
+        saveLocation();
+
+        // reset inputs
+        labelInput.setText("");
+        latInput.setText("");
+        longInput.setText("");
+    }
+
+    public void onFinish(View view) {
+        saveLocation();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/socialcompass/LocationService.java
@@ -0,0 +1,30 @@
+package com.example.socialcompass;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class LocationService {
+    private MutableLiveData<Location> Location;
+    private ExecutorService backgroundThreadExecutor = Executors.newSingleThreadExecutor();
+    private Future<?> registerFuture;
+
+    protected LocationService() {
+        this.Location = new MutableLiveData<>();
+    }
+
+    public void registerLocation(Location input) {
+        this.registerFuture = backgroundThreadExecutor.submit(() -> {
+
+            this.Location.postValue(input);
+        });
+    }
+
+    public MutableLiveData<Location>  getLocations(){
+        return this.Location;
+    }
+}
