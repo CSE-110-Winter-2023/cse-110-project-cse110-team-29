0334bee2ea388b6431530394d8eeb693a0f7055d
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2023-02-12 15:33:40 -0800

    updated circular view

--- app/src/main/java/com/example/socialcompass/CircularActivity.java
+++ app/src/main/java/com/example/socialcompass/CircularActivity.java
@@ -14,17 +14,38 @@ public class CircularActivity extends AppCompatActivity {
         setContentView(R.layout.activity_circular);
         loadInput();
     }
-    public void loadInput(){
-        SharedPreferences prefs = getSharedPreferences("data",MODE_PRIVATE);
+
+    public void loadInput() {
+        SharedPreferences prefs = getSharedPreferences("data", MODE_PRIVATE);
+
         String label = prefs.getString("parentsLabel","");
         Float lat = prefs.getFloat("parentsLat",0);
         Float longi = prefs.getFloat("parentsLong",0);
+
         TextView labelView = findViewById(R.id.label);
         TextView latView = findViewById(R.id.lat);
         TextView longiView = findViewById(R.id.longi);
+
         labelView.setText(label);
         latView.setText(lat.toString());
         longiView.setText(longi.toString());
     }
 
+    private double angleFromCoordinate(double lat1, double long1, double lat2,
+                                       double long2) {
+
+        double dLon = (long2 - long1);
+
+        double y = Math.sin(dLon) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
+                * Math.cos(lat2) * Math.cos(dLon);
+
+        double brng = Math.atan2(y, x);
+
+        brng = Math.toDegrees(brng);
+        brng = (brng + 360) % 360;
+        brng = 360 - brng; // count degrees counter-clockwise - remove to make clockwise
+
+        return brng;
+    }
 }
