14b290c2bf12e10dd66a30dd20fcc4c7df668c73
elllisonLi <yul054@ucsd.edu>
2023-02-15 18:09:56 -0800

    CircularActivity

--- app/src/main/java/com/example/socialcompass/CircularActivity.java
+++ app/src/main/java/com/example/socialcompass/CircularActivity.java
@@ -2,31 +2,75 @@ package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CircularActivity extends AppCompatActivity {
     private LocationService locationService;
+    private OrientationServiceS orientationService;
+    private float orientationAngel;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_circular);
+//        locationService = LocationService.singleton(this);
+//        locationService.getLocation().observe(this, loc -> {
+//            loadInput(loc.first, loc.second);
+//            // Utilities.showAlert(this, String.valueOf(loc.first) + "," + String.valueOf(loc.second));
+//        });
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circular);
+
+//        loadInput();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
         locationService = LocationService.singleton(this);
         locationService.getLocation().observe(this, loc -> {
             loadInput(loc.first, loc.second);
             // Utilities.showAlert(this, String.valueOf(loc.first) + "," + String.valueOf(loc.second));
         });
+
+        orientationService = new OrientationServiceS(this);
+        ImageView image = findViewById(R.id.clockFace);
+
+        orientationService.getOrientation().observe(this, orientation -> {
+            orientationAngel = orientation;
+            orientationSet(image, orientationAngel);
+        });
+    }
+
+    private void orientationSet(ImageView image, float degree) {
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) image.getLayoutParams();
+        if (degree < 0) {
+            degree += 2 * Math.PI;
+        }
+        layoutParams.circleAngle = (float) (180 * degree / (Math.PI));
+        image.setLayoutParams(layoutParams);
     }
 
     public void loadInput(double myLat, double myLong) {
         SharedPreferences prefs = getSharedPreferences("data", MODE_PRIVATE);
 
-        String label = prefs.getString("parentsLabel","");
-        Float latitude = prefs.getFloat("parentsLat",0);
-        Float longitude = prefs.getFloat("parentsLong",0);
+        String label = prefs.getString("parentsLabel", "");
+        Float latitude = prefs.getFloat("parentsLat", 0);
+        Float longitude = prefs.getFloat("parentsLong", 0);
 
         TextView labelView = findViewById(R.id.label);
         //get the label from user input
@@ -38,7 +82,9 @@ public class CircularActivity extends AppCompatActivity {
         // coordinates of Geisel Library
         // final double myLat = 32.881174;
         // final double myLong = -117.2378661;
-        layoutParams.circleAngle = (float) angle_in_activity(myLat, myLong, (double)latitude, (double)longitude);
+        layoutParams.circleAngle = (float) angle_in_activity(myLat, myLong, (double) latitude, (double) longitude);
+        labelView.setLayoutParams(layoutParams);
+
     }
 
     private double angle_in_activity(double lat1, double long1, double lat2,
