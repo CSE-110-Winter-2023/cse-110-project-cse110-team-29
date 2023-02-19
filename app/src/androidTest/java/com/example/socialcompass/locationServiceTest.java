d73b3976642c3c0569704fa0886d8731bfe8741f
johnsonli010801 <97709195+johnsonli010801@users.noreply.github.com>
2023-02-19 02:34:49 -0800

    adding two mocking Junit test for location service and orientation service

---
+++ app/src/androidTest/java/com/example/socialcompass/locationServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.socialcompass;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.util.Pair;
+import androidx.lifecycle.MutableLiveData;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+
+public class locationServiceTest {
+    @Test
+    /**
+     * This test is singlely for testing the passing in the mock data into
+     * location service class and observe the angle that parent label change after
+     * the location change.
+     */
+    public void testForLocationService(){
+        var pair = new Pair<Double,Double>(12.0,24.0);
+        var scenario = ActivityScenario.launch(CircularActivity.class);
+        scenario.onActivity(activity -> {
+            var locationService = LocationService.singleton(activity);
+            var mockLocation = new MutableLiveData<Pair<Double,Double>>();
+            locationService.setMockOrientationData(mockLocation);
+            activity.reobserveLocation();
+            mockLocation.setValue(pair);
+            TextView parent = activity.findViewById(R.id.ParentHome);
+            var prefs = activity.getSharedPreferences("data", Context.MODE_PRIVATE);
+            var expected = (float)(180*-Utilities.angleInActivity(pair.first,pair.second,prefs.getFloat("parentsLat", 0),prefs.getFloat("parentsLong", 0))/Math.PI);
+            var layout = (ConstraintLayout.LayoutParams) parent.getLayoutParams();
+            var actual  = layout.circleAngle;
+            assertEquals(expected,actual,0);
+        });
+    }
+}
