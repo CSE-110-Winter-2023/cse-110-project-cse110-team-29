a973347170762d02ff6a7b05a7273054e418de9a
johnsonli010801 <97709195+johnsonli010801@users.noreply.github.com>
2023-02-19 21:30:24 -0800

    Update InputActivity.java

--- app/src/main/java/com/example/socialcompass/InputActivity.java
+++ app/src/main/java/com/example/socialcompass/InputActivity.java
@@ -1,7 +1,7 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
